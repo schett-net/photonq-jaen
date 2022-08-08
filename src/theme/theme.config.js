ab329a139adddbb6be17981082f479f1fd990564
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-08-08 11:36:50 +0000

    add theme

---
+++ src/theme/theme.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  primary: '#0063A6',
+  secondary: '#ffffff',
+  primaryLight: '#AFCDE3',
+  grey: '#666666',
+  secondaryDark: '#2D2F38',
+  primaryDark: '#393B43',
+  primaryLightBg: 'rgba(38, 119, 178, 0.04)'
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./src/theme/theme.config')
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}'
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        'primary-light': '#AFCDE3',
+        ...colors
+      },
+      maxWidth: {
+        max: '1400px'
+      },
+      width: {
+        'filter-button': '142px',
+        'filter-button-sm': '81px'
+      },
+      maxHeight: {
+        hero: '1100px'
+      },
+      padding: {
+        navbar: '80px'
+      }
+    }
+  },
+  variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
