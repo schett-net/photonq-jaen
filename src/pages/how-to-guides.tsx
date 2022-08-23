01e5ae93e0f53d05b6b1d4360b6f1692cc1fcf7d
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-08-23 11:53:42 +0000

    add page layout to how to guides

--- src/pages/how-to-guides.tsx
+++ src/pages/how-to-guides.tsx
@@ -1,16 +1,15 @@
-import {connectPage, Field} from '@jaenjs/jaen'
-import {graphql} from 'gatsby'
+import { connectPage, Field } from '@jaenjs/jaen'
+import { graphql, PageProps } from 'gatsby'
 import React from 'react'
 import GuidePreview from '../components/DocumentationPreview/GuidePreview'
-import {GuidePreviewGrid} from '../components/DocumentationPreview/GuidePreviewGrid'
+import { GuidePreviewGrid } from '../components/DocumentationPreview/GuidePreviewGrid'
 import GuideFilter from '../components/GuideFilter/GuideFilter'
-import {useGuideFilter} from '../components/GuideFilter/hook.guideFilter'
+import { useGuideFilter } from '../components/GuideFilter/hook.guideFilter'
 import MaxWidthContainer from '../components/Layout/MaxWidthContainer'
 import NavbarPadding from '../components/Layout/NavbarPadding'
+import PageLayout from '../components/Layout/PageLayout'
 import Section from '../components/Section/Section'
 import SectionHeader from '../components/Section/SectionHeader'
-
-interface PageProps {}
 
 export function HowToGuidesPage(props: PageProps) {
   const {
@@ -24,59 +23,61 @@ export function HowToGuidesPage(props: PageProps) {
   })
 
   return (
-    <div className={'flex flex-col items-center'}>
-      <MaxWidthContainer className={'space-y-12'}>
-        <NavbarPadding />
-        <Section>
-          <div className={'flex flex-col items-center space-y-5'}>
-            <SectionHeader
-              text={
+    <PageLayout pathname={props.path}>
+      <div className={'flex flex-col items-center'}>
+        <MaxWidthContainer className={'space-y-12'}>
+          <NavbarPadding />
+          <Section>
+            <div className={'flex flex-col items-center space-y-5'}>
+              <SectionHeader
+                text={
+                  <Field.Text
+                    name="how-to-guide-header"
+                    defaultValue={'Documentation and How-To-Guides'}
+                  />
+                }
+              />
+              <p className={'md:w-8/12 md:text-center'}>
                 <Field.Text
-                  name="how-to-guide-header"
-                  defaultValue={'Documentation and How-To-Guides'}
-                />
-              }
-            />
-            <p className={'md:w-8/12 md:text-center'}>
-              <Field.Text
-                name="how-to-guide-text"
-                defaultValue={`
+                  name="how-to-guide-text"
+                  defaultValue={`
 Each of these guides and documentation contains advice and
 research to give you guidelines for your successful quantum
 computing experiment.`}
-              />
-            </p>
-          </div>
-        </Section>
-        <GuideFilter
-          currentCategory={currentCategory}
-          setCurrentCategory={setCurrentCategory}
-        />
-        <Section>
-          {isLoading ? (
-            <div className={'flex justify-center'}>
-              <span className="h-10 w-10 animate-ping inline-flex rounded-full bg-primary opacity-75" />
-              <p>Loading...</p>
+                />
+              </p>
             </div>
-          ) : (
-            <div className={'pb-8'}>
-              <GuidePreviewGrid>
-                {filteredGuides.map(child =>
-                  withJaenPage(
-                    child.id,
-                    <GuidePreview
-                      key={child.id}
-                      index="how-to-guides"
-                      slug={child.slug!}
-                    />
-                  )
-                )}
-              </GuidePreviewGrid>
-            </div>
-          )}
-        </Section>
-      </MaxWidthContainer>
-    </div>
+          </Section>
+          <GuideFilter
+            currentCategory={currentCategory}
+            setCurrentCategory={setCurrentCategory}
+          />
+          <Section>
+            {isLoading ? (
+              <div className={'flex justify-center'}>
+                <span className="h-10 w-10 animate-ping inline-flex rounded-full bg-primary opacity-75" />
+                <p>Loading...</p>
+              </div>
+            ) : (
+              <div className={'pb-8'}>
+                <GuidePreviewGrid>
+                  {filteredGuides.map(child =>
+                    withJaenPage(
+                      child.id,
+                      <GuidePreview
+                        key={child.id}
+                        index="how-to-guides"
+                        slug={child.slug!}
+                      />
+                    )
+                  )}
+                </GuidePreviewGrid>
+              </div>
+            )}
+          </Section>
+        </MaxWidthContainer>
+      </div>
+    </PageLayout>
   )
 }
 
@@ -88,5 +89,5 @@ export const query = graphql`
 
 export default connectPage(HowToGuidesPage, {
   displayName: 'HowToGuidesPage',
-  children: ['HowToGuide'],
+  children: ['HowToGuide']
 })
