42e8731dfdf1bccfc0b83f7e61ed088df04e55bb
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-09-06 17:57:44 +0000

    wip: update maxWith of some pages

--- src/pages/my-projects.tsx
+++ src/pages/my-projects.tsx
@@ -1,6 +1,8 @@
 import {connectPage, Field} from '@jaenjs/jaen'
+import {Container} from '@mui/system'
 import {graphql, PageProps} from 'gatsby'
 import React, {useState} from 'react'
+import ContentContainer from '../components/Layout/ContentContainer'
 import NavbarPadding from '../components/Layout/NavbarPadding'
 import PageLayout from '../components/Layout/PageLayout'
 import SectionHeader from '../components/Layout/SectionHeader'
@@ -19,40 +21,47 @@ export function MyProjectPage(props: PageProps) {
 
   return (
     <PageLayout pathname={props.path}>
-      <NavbarPadding />
-      <SectionHeader
-        withVerticalSpacing
-        header={
-          <Field.Text name="sectionHeader" defaultValue={'My Experiments'} />
-        }
-        onClick={() =>
-          setAddExperimentDialogProps(prev => ({...prev, open: true}))
-        }
-        withButton
-        buttonText={'Add new Experiment'}
-      />
-      <ProjectExperimentDataContextProvider>
-        <MainTable setAddExperimentDialogProps={setAddExperimentDialogProps} />
-        {/*---FOR RELEASE 1 NOT RELEVANT---*/}
-        {/*<SystemDialog*/}
-        {/*  setIsOpen={setIsProjectDialogOpen}*/}
-        {/*  isOpen={isAddProjectDialogOpen}*/}
-        {/*  buttonText={"Add"}*/}
-        {/*  title={"Add new Project"}*/}
-        {/*  label={"Project Name"}*/}
-        {/*  variant={"projects"}*/}
-        {/*/>*/}
-        <SystemDialog
-          setIsOpen={(value: boolean) =>
-            setAddExperimentDialogProps(prev => ({...prev, open: value}))
+      <Container maxWidth="xl">
+        <NavbarPadding />
+        <SectionHeader
+          withVerticalSpacing
+          header={
+            <Field.Text name="sectionHeader" defaultValue={'My Experiments'} />
           }
-          variant={'experiments'}
-          isOpen={addExperimentDialogProps.open}
-          buttonText={'Add'}
-          title={'Add new Experiment'}
-          label={'Experiment Name'}
+          onClick={() =>
+            setAddExperimentDialogProps(prev => ({...prev, open: true}))
+          }
+          withButton
+          buttonText={'Add new Experiment'}
         />
-      </ProjectExperimentDataContextProvider>
+        <ContentContainer>
+          <ProjectExperimentDataContextProvider>
+            <MainTable
+              setAddExperimentDialogProps={setAddExperimentDialogProps}
+            />
+            {/*---FOR RELEASE 1 NOT RELEVANT---*/}
+            {/*<SystemDialog*/}
+            {/*  setIsOpen={setIsProjectDialogOpen}*/}
+            {/*  isOpen={isAddProjectDialogOpen}*/}
+            {/*  buttonText={"Add"}*/}
+            {/*  title={"Add new Project"}*/}
+            {/*  label={"Project Name"}*/}
+            {/*  variant={"projects"}*/}
+            {/*/>*/}
+            <SystemDialog
+              setIsOpen={(value: boolean) =>
+                setAddExperimentDialogProps(prev => ({...prev, open: value}))
+              }
+              variant={'experiments'}
+              isOpen={addExperimentDialogProps.open}
+              buttonText={'Add'}
+              title={'Add new Experiment'}
+              label={'Experiment Name'}
+            />
+          </ProjectExperimentDataContextProvider>
+        </ContentContainer>
+        <NavbarPadding />
+      </Container>
     </PageLayout>
   )
 }
